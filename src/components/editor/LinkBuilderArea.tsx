823028fcb30f97cb4efe90dd9197051064dc6231
u53f <scalewithusef@gmail.com>
2023-08-27 17:40:36 +0100

    add: d&d feature

--- src/components/editor/LinkBuilderArea.tsx
+++ src/components/editor/LinkBuilderArea.tsx
@@ -16,8 +16,23 @@ import SortableLinkBuilder from "./SortableLinkBuilder";
 function LinkBuilderArea() {
   const [items, setItems] = useState(["github", "youtube", "linkedin"]);
 
-  const handleDragEnd = () => {
+  const handleDragEnd = (event: any) => {
     console.log("Drag end");
+
+    const { active, over } = event;
+    console.log("ACTIVE: " + active.id);
+    console.log("OVER :" + over.id);
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+        console.log(arrayMove(items, activeIndex, overIndex));
+        return arrayMove(items, activeIndex, overIndex);
+        // items: [2, 3, 1]   0  -> 2
+        // [1, 2, 3] oldIndex: 0 newIndex: 2  -> [2, 3, 1]
+      });
+    }
   };
 
   const sensors = useSensors(
